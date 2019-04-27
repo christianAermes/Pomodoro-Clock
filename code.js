dd818f7bf52263ae9b584bde4f34e6d1f91ac557
Christian Aerms <35460812+christianAermes@users.noreply.github.com>
2019-04-27 22:37:12 +0200

    Added Files

---
+++ code.js
@@ -0,0 +1,99 @@
+$(document).ready(function(){
+  var counter = 0;
+  var timeLeft = 25*60;
+  var running = false;
+  function pad(d){
+      return (d<10)? "0"+d.toString() : d.toString()
+  }
+  function convertSeconds(s){
+    var hrs = Math.floor(s/3600.0)
+    hrs = pad(hrs)
+    var min = Math.floor((s-hrs*3600)/60.0)
+    min = pad(min)
+    var sec = Math.floor((s-hrs*3600-min*60))
+    sec = pad(sec)
+    return hrs + " : " + min + " : " + sec
+  }
+  
+  $("#hrsPlus").click(function(){
+    if (!running){
+      if (timeLeft+3600 <= 24*3600){
+        timeLeft += 3600
+        $("#timer").html(convertSeconds(timeLeft - counter))
+      }
+    }
+  })
+  $("#minPlus").click(function(){
+    if (!running){
+      if (timeLeft+60 <= 24*3600){
+        timeLeft += 60
+        $("#timer").html(convertSeconds(timeLeft - counter))
+      }
+    }
+  })
+  $("#secPlus").click(function(){
+    if (!running){
+      if (timeLeft+1 <= 24*3600){
+        timeLeft += 1
+        $("#timer").html(convertSeconds(timeLeft - counter))
+      }
+    }
+  })
+  $("#hrsMinus").click(function(){
+    if (!running){
+      if (timeLeft-3600 > 0){
+        timeLeft -= 3600
+        $("#timer").html(convertSeconds(timeLeft - counter))
+      }
+    }
+  })
+  $("#minMinus").click(function(){
+    if (!running){
+      if (timeLeft-60 > 0){
+        timeLeft -= 60
+        $("#timer").html(convertSeconds(timeLeft - counter))
+      }
+    }
+  })
+  $("#secMinus").click(function(){
+    if (!running){
+      if (timeLeft-1 > 0){
+        timeLeft -= 1
+        $("#timer").html(convertSeconds(timeLeft - counter))
+      }
+    }
+  })
+  
+  
+  
+  $("#timer").html(convertSeconds(timeLeft - counter))
+
+  $("#startClock").click(function(){
+    running = true;
+    function timeIt(){
+      counter++;
+      $("#timer").html(convertSeconds(timeLeft - counter))
+      if(timeLeft - counter == 0){
+        clearInterval(stopWatch)
+        $("#timer").css("background", "red")
+      } else {
+        $("#timer").css("background", "#92a05f")
+      }
+    }
+    var stopWatch = setInterval(timeIt, 1000)
+    $("#stopClock").click(function(){
+      running = false;
+      clearInterval(stopWatch);
+    })
+    $("#resetClock").click(function(){
+      clearInterval(stopWatch)
+      timeLeft = 25*60;
+      counter = 0;
+      running = false;
+      $("#timer").html(convertSeconds(timeLeft - counter))
+      $("#timer").css("background", "#92a05f")
+    })
+  })
+    
+  
+})
